d41ad74d61339aea689b6a8f9f70062a867b41f4
nadiacandraa <79911218+nadiacandraa@users.noreply.github.com>
2021-10-25 23:56:13 +0900

    Create Login.js

---
+++ version2/Login.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import "./App.css";
+//import Footer from './Footer';
+//import NavBar from './NavBar';
+//import Search from './Search';
+
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+
+    if (username === "user" && password === "web_dev") {
+        alert("You have successfully logged in.");
+        window.location.reload();
+    } else {
+        loginErrorMsg.style.opacity = 1;
+    }
+})
+
+class Login extends React.Component{
+    render(){
+        return(
+            <>   
+            
+         
+            </>
+        );
+    }
+}
+
+export default Login;
