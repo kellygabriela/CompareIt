3253597550d43b6c7aacc9a9999d8ca07b79c3cc
debdeb18 <80741322+debdeb18@users.noreply.github.com>
2021-10-26 19:06:56 +0900

    Update Login.js

--- version2/Login.js
+++ version2/Login.js
@@ -1,31 +1,43 @@
 import React from 'react';
 import "./App.css";
 
-const loginForm = document.getElementById("login-form");
-const loginButton = document.getElementById("login-form-submit");
-const loginErrorMsg = document.getElementById("login-error-msg");
-
-loginButton.addEventListener("click", (e) => {
-    e.preventDefault();
-    const username = loginForm.username.value;
-    const password = loginForm.password.value;
-
-    if (username === "guest" && password === "guest") {
-        alert("You have successfully logged in.");
-        window.location.reload();
-    } else {
-        loginErrorMsg.style.opacity = 1;
+class Login extends React.Component {
+    loginfunction = () => {
+        const loginForm = document.getElementById("login-form");
+    
+        const username = loginForm.username.value;
+        const password = loginForm.password.value;
+        console.log("sumbit clicked")
+    
+        if (username === "guest" && password === "guest") {
+            alert("You have successfully logged in.");
+            return this.props.history.push('/')
+        } else {
+            alert("Invalid username and/or password");
+            document.getElementById("login-form").reset()
+        }
     }
-})
 
-class Login extends React.Component{
-    render(){
-        return(
-            <>   
-            
-         
-            </>
-        );
+    render() {
+        return (
+        <>
+        <main id="main-holder">
+            <h1 id="login-header">Login</h1>
+
+            <div id="login-error-msg-holder">
+                <p id="login-error-msg">Invalid username <span id="error-msg-second-line">and/or password</span></p>
+            </div>
+        
+            <form id="login-form">
+                <label>
+                    <input type="text" name="username" id="username-field" class="login-form-field" placeholder="Username" /> <br/>
+                    <input type="password" name="password" id="password-field" class="login-form-field" placeholder="Password" /> <br/>
+                    <button type="submit" value="Login" id="login-form-submit" onClick={this.loginfunction}> Login </button>
+                </label>
+            </form>
+        </main>
+    </>
+  );
     }
 }
 
