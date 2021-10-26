42653e66e2a2885045057bb763b32e660bbc5e8a
debdeb18 <80741322+debdeb18@users.noreply.github.com>
2021-10-27 04:11:26 +0900

    Create ProductList.js

---
+++ version2/ProductList.js
@@ -0,0 +1,50 @@
+import "./Search.css"
+
+function ProductList() {
+    let hpItems = [];
+
+    const loadItems = () => {
+      
+            fetch('ProdData.json', {
+                headers : { 
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                }
+            })
+            .then(function(response){
+                console.log(response);
+                console.log("");
+                return response.json();
+            })
+    
+            .then(function(myJson) {
+                hpItems = myJson;
+                console.log(hpItems);
+                displayItems(hpItems);
+            });
+    };
+
+    loadItems();
+
+    const displayItems = (items) => {
+        const htmlString = items
+            .map((item) => {
+                console.log("yuhuu")
+                return `
+                <div class="col-3">
+                    <h2>${item.title}</h2>
+                    <p>Price: ${item.price}</p>
+                    <img src=${item.picture} alt=${item.title} class="item-img"></img>
+                    <a href=${item.link} class="btn"> Shop Now &#8594;</a>
+                </div>
+            `;
+            {/* harusnya hrefnya nggak item.link, tpi ke product detail */}
+        })
+        .join('');
+        document.getElementById('HomeList').innerHTML = htmlString;
+    };
+
+    return null;
+}
+
+export default ProductList;
