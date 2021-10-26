afd5052bcc52a5b6bd1996bbde200f2efd821845
kellygabriela <79897993+kellygabriela@users.noreply.github.com>
2021-10-26 21:02:12 +0900

    Update Products.js

--- version2/Products.js
+++ version2/Products.js
@@ -26,24 +26,49 @@ class Products extends React.Component{
                 </div>
             </div>
             <div class="container-s">
-                <h1 class="titleMain">What are you searching?</h1>
+                <h1 class="titleMain"> What are your searching for? </h1>
+                <div class ="search-container">
                 <div id="searchWrapper">
                     <input
                         type="text"
                         name="searchBar"
                         id="searchBar"
-                        placeholder="Search here"
+                        placeholder="search here..."
                     />
+                
+                </div>
                 </div>
                 <ul id="ItemsList"></ul>
                 <script>
                     <Search />
                 </script>
             </div>
+            <div class="categories">
+                <div class="small-container">
+                    <div class="row">
+                        <div class="col-3">
+                            <img src="https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/47922912-39af-49e3-ac43-d11aac1ac7ba/air-jordan-1-mid-shoes-scZZ99.png" class="item-img" alt=" "/>
+                            <a href="/productpage" class="btn"> Shop now &#8594;</a>
+                           
+                            
+                        </div>
+                        <div class="col-3">
+                            <img src="https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/b9bc37ba-c4e3-4fdd-b7c5-f62de33cbbe8/air-jordan-1-mid-shoes-N1GfGx.png" class="item-img" alt=" "/>
+                            <a href="/productpage" class="btn"> Shop now &#8594;</a>
+                        </div>
+                        <div class="col-3">
+                            <img src="https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/058a81e7-963c-4236-a350-6599ce5d1fec/air-jordan-1-mid-se-shoes-6d14MT.png" class="item-img" alt=" "/>
+                            <a href="/productpage" class="btn"> Shop now &#8594;</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <Footer />
             </>
+            
         );
     }
 }
 
 export default Products;
+
