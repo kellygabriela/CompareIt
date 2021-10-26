ca3e0c0540c9549a8e5ff9f613afeb29a6adaa76
nadiacandraa <79911218+nadiacandraa@users.noreply.github.com>
2021-10-26 10:41:15 +0900

    Update Login.js

--- version2/Login.js
+++ version2/Login.js
@@ -13,7 +13,7 @@ loginButton.addEventListener("click", (e) => {
     const username = loginForm.username.value;
     const password = loginForm.password.value;
 
-    if (username === "user" && password === "web_dev") {
+    if (username === "guest" && password === "guest") {
         alert("You have successfully logged in.");
         window.location.reload();
     } else {
