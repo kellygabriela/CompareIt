f600ef341bb66f1c89cf0846b27be741ec4b7477
kellygabriela <79897993+kellygabriela@users.noreply.github.com>
2021-10-27 14:17:00 +0900

    Update App.js

--- version2/App.js
+++ version2/App.js
@@ -5,9 +5,10 @@ import Home from './Home';
 import {Route} from "react-router-dom";
 import About from './About';
 import Contacts from './Contacts';
-
+import ProductList from './ProductList';
 //new
 import Login from './Login';
+import Signup from './Signup';
 
 function App(){
     return(
@@ -18,6 +19,8 @@ function App(){
             <Route exact path="/contacts" component={Contacts} />
             {/*new*/}
             <Route exact path="/login" component={Login} />
+            <Route exact path="/productlist" component={ProductList} /> 
+            <Route exact path="/signup" component={Signup} /> 
         </>
     );
 }
