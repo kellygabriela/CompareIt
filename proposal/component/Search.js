c6f4bf210ee0ef432267cc07a84c088fb3b6e03d
debdeb18 <80741322+debdeb18@users.noreply.github.com>
2021-11-19 20:12:15 +0900

    Update Search.js

--- proposal/component/Search.js
+++ proposal/component/Search.js
@@ -1,60 +1,89 @@
 import "./Search.css"
 import "./App.css"
+import SearchBar from "./SearchBar";
+import { BrowserRouter as Router } from 'react-router-dom';
+import { useState } from 'react';
 
-function Search() {
-    let hpItems = [];
+//array that hold the product from database
+let dbItems = []
 
-    const loadItems = () => {
+//filter items based on searchBar input (query)
+const filterItems = (dbItems, query) => {
+    if(query === null){
+        return dbItems;
+    }
+
+    return dbItems.filter((item) => {
+        const searchedItem = item.keyword.toLowerCase();
+        return searchedItem.includes(query);
+    })
+}
+
+//load data from database to dbItems array
+const loadItems = () => {
       
-            fetch('ProdData.json', {
-                headers : { 
-                    'Content-Type': 'application/json',
-                    'Accept': 'application/json'
-                }
-            })
-            .then(function(response){
-                console.log(response);
-                console.log("");
-                return response.json();
-            })
-    
-            .then(function(myJson) {
-                hpItems = myJson;
-                console.log(hpItems);
-                displayItems(hpItems);
-            });
-    };
+    fetch('ProdData.json', {
+        headers : { 
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        }
+    })
 
-    loadItems();
+    .then(function(response){
+        console.log(response);
+        console.log("");
+        return response.json();
+    })
 
-    const displayItems = (items) => {
-        const htmlString = items
-            .map((item) => {
-                return `
+    .then(function(myJson) {
+        dbItems = myJson;
+        console.log(dbItems);
+        displayItems(dbItems);
+    });
+};
+
+loadItems();
+
+const displayItems = (items) => {
+    items.map((item) => {
+            return `
+            <li class="items">
+                <h2>${item.title}</h2>
+                <h4>Price: ${item.price}</h4>
+                <a href=${item.link} class="btn">Shop</a>
+                <img src=${item.picture}></img>
+            </li>
+        `;
+    })
+    .join('');
+};
+
+const Search = () => {
+    const { search } = window.location;
+    const query = new URLSearchParams(search).get('searchBar');
+    const [searchQuery, setSearchQuery] = useState(query || '');
+    const filteredItems = filterItems(dbItems, searchQuery);             
+
+    return(
+        <Router>
+        <div>
+            <SearchBar 
+                searchQuery={searchQuery}
+                setSearchQuery={setSearchQuery}
+            />
+            <ul id="ItemsList">
+                {filteredItems.map(item => (
                 <li class="items">
-                    <h2>${item.title}</h2>
-                    <h4>Price: ${item.price}</h4>
-                    <a href=${item.link} class="btn">Shop</a>
-                    <img src=${item.picture}></img>
+                    <h2>{item.title}</h2>
+                    <h4>Price: {item.price}</h4>
+                    <a href={item.link} class="btn">Shop</a>
+                    <img src={item.picture} alt=" "></img>
                 </li>
-            `;
-        })
-        .join('');
-        document.getElementById('ItemsList').innerHTML = htmlString;
-
-        document.getElementById('searchBar').addEventListener('keyup', (e) => {
-            const searchString = e.target.value.toLowerCase();
-
-            const filteredItems = hpItems.filter((item) => {
-                return (
-                    item.keyword.toLowerCase().includes(searchString)
-                );
-            });
-            displayItems(filteredItems);
-        });
-    };
-
-    return null;
+                ))}
+            </ul>
+        </div>
+        </Router>
+    )
 }
 
 export default Search;
