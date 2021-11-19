135550c1c279f3eb6e3a68921f74d47409ca1da3
nadiacandraa <79911218+nadiacandraa@users.noreply.github.com>
2021-11-19 15:34:32 +0900

    Update Signup.js

--- proposal/Signup.js
+++ proposal/Signup.js
@@ -12,6 +12,7 @@ class Signup extends React.Component {
         const username = loginForm.username.value;
         const password = loginForm.password.value;
         const email = loginForm.email.value;
+        const retypepassword = loginForm.retypepassword.value;
 
         
         if( email === "guest@example.com") {
@@ -34,7 +35,15 @@ class Signup extends React.Component {
             document.querySelector( "#password-field").setCustomValidity( "Please fill in this field" );
             document.querySelector( "form" ).reportValidity();
             document.getElementById("login-form").reset();
-        } else {
+        } else if (retypepassword !== password) {
+            document.querySelector( "#retypepassword-field").setCustomValidity( "Password is not the same" );
+            document.querySelector( "form" ).reportValidity();
+            document.getElementById("login-form").reset();
+        } else if (password.length <= 7) {
+            document.querySelector( "#retypepassword-field").setCustomValidity( "Password length must be atleast 8 characters" );
+            document.querySelector( "form" ).reportValidity();
+            document.getElementById("login-form").reset();
+        }else {
             alert("You have successfully register your account.");
             return this.props.history.push('/Login');
         }
@@ -55,6 +64,7 @@ class Signup extends React.Component {
                     <input type="text" name="email" id="email-field" class="login-form-field" placeholder="email" /> <br/>
                     <input type="text" name="username" id="username-field" class="login-form-field" placeholder="Username" /> <br/>
                     <input type="password" name="password" id="password-field" class="login-form-field" placeholder="Password" /> <br/>
+                    <input type="password" name="retypepassword" id="retypepassword-field" class="login-form-field" placeholder="Confirm Password" /> <br/>
                     <button type="submit" value="Login" id="login-form-submit" onClick={this.signupfunction}> Sign up </button>
                     <h4 class="loginbtn"><a href="/login"> back to login </a></h4>
                 </label>
