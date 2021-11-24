32b83b48c1d73fa00a2e1e507447554b88642133
debdeb18 <80741322+debdeb18@users.noreply.github.com>
2021-11-24 19:28:50 +0900

    Update NavBar.js

--- proposal/component/NavBar.js
+++ proposal/component/NavBar.js
@@ -1,27 +1,29 @@
 import React from 'react';
 import "./App.css";
-import {Link} from "react-router-dom";
+import {NavLink, Link} from "react-router-dom";
 
 class NavBar extends React.Component{
     menutoggle = () => {
         if (document.getElementById('MenuItems').style.maxHeight === "0px") {
-            document.getElementById('MenuItems').style.maxHeight = "200px"
+            document.getElementById('MenuItems').style.maxHeight = "200px";
+            document.getElementById('dummy-div').style.height = "130px";
         } else {
-            document.getElementById('MenuItems').style.maxHeight = "0px"
+            document.getElementById('MenuItems').style.maxHeight = "0px";
+            document.getElementById('dummy-div').style.height = "0px";
         };
-        console.log(document.getElementById('MenuItems').style.maxHeight);
     }
 
     render(){
-        return(      
+        return(   
+            <>   
             <div class="navbar">
                 <div class="app-logo">
                     <Link to="/"><img src="https://images-ext-2.discordapp.net/external/6XpAv9LnRxp2-KK3GT_CsUYBukjnv3CUr7EEnr_2kEo/https/blogger.googleusercontent.com/img/a/AVvXsEisX33zecaRLzHTN1yd57kS5yE_WJImMJ2HBTdtvO2unfY7XGh-bj2lnhXg7GQ5BWf_wldKqBrIh6KgLGDJ5XtFwZIbGSw5nKbkk5W_cR0NVZJvVjUdCOErYcnZ0gVKUqkmekCWsErp1ca3Hjb39JNV2DT4e3NTEkhP8tahOniaTHpx_kwdk9mNGxWX5A%3Ds504?width=495&height=181" alt=" " width="200px" /></Link>
                 </div>
                 <nav>
                     <ul id="MenuItems" class="MenuItems" onClick="document.getElementById('MenuItems').style.maxHeight = '0px'">
-                        <li><Link to="/">Home</Link></li>
-                        <li><Link to="/products">Products</Link></li>
+                        <li><NavLink to="/">Home</NavLink></li>
+                        <li><NavLink to="/products">Products</NavLink></li>
                         {/*<div class="dropdown">
                         <li class="dropbtn"></li>
                             <div class="dropdown-content">
@@ -30,7 +32,7 @@ class NavBar extends React.Component{
                                 <a href="http://www.google.com">kaos kali</a>
                             </div>
         </div>*/}
-                        <li><Link to="/about">About</Link></li>
+                        <li><NavLink to="/about">About</NavLink></li>
                         {/*<div class="dropdown">
                             <li class="dropbtn"></li>
                             <div class="dropdown-content">
@@ -42,9 +44,10 @@ class NavBar extends React.Component{
                     </ul>
                 </nav>
                 {/*<img src="https://i.pinimg.com/originals/15/4f/df/154fdf2f2759676a96e9aed653082276.png" class="nav-icon" width="30px" height="30px" alt=" " />*/}
-                <img src="https://cdn.icon-icons.com/icons2/916/PNG/512/Menu_icon_icon-icons.com_71858.png" class="menu-icon" onClick={this.menutoggle} alt=" "/>
-                <li class="login-navbar"><Link to="/Login">Logout</Link></li>
+                <img src="phone-menu-icon-navbar.png" class="menu-icon" onClick={this.menutoggle} alt=" "/>
+                <li class="login-navbar"><NavLink to="/Login">Logout</NavLink></li>
             </div>
+            </>
         );
     }
 }
