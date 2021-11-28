c34de1ff29eb4affc768875ff0b2d39136739287
debdeb18 <80741322+debdeb18@users.noreply.github.com>
2021-11-28 18:26:00 +0900

    version4

--- src/About.js
+++ src/About.js
@@ -7,6 +7,7 @@ class About extends React.Component{
             <>
             <div class="container" style={{minHeight: "350px"}}>
                 <h1>This is About Page</h1>
+                <p>write description about the team and project <br/> as well as introduction about the app </p>
             </div>
             </>
         );
