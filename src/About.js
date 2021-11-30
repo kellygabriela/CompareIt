4451c2b6679d556db70b98b9e7cd80c810b95248
kellygabriela <79897993+kellygabriela@users.noreply.github.com>
2021-11-30 12:23:26 +0900

    Add files via upload

--- src/About.js
+++ src/About.js
@@ -1,14 +1,39 @@
 import React from 'react';
-import "./component/App.css";
+import './About.css';
+
 
 class About extends React.Component{
     render(){
         return(
             <>
-            <div class="container" style={{minHeight: "350px"}}>
-                <h1>This is About Page</h1>
-                <p>write description about the team and project <br/> as well as introduction about the app </p>
+            <div class="about-section" style={{minHeight: "350px"}}>
+                <div class ="inner-container">
+               
+            <h1>About us</h1>
+                <p>Compare it is designed to help users compare prices of items within various websites. </p>
+                <p> With a click of a button, we can help the users to get the best prices available in the market.  </p>
+                <p> compare your price now!</p>
+                      
+
+                </div>
+             
             </div>
+            <div class="developer-container">
+                <h2 class="title">About the Developers</h2>
+                <div class="developer-section">
+                <small> Angelina Deby.........................Frontend Developer</small><br></br><br></br>
+                <small> Kelly Gabriela..........................Frontend Developer</small><br></br><br></br>
+                <small> Nadia Candra..........................Frontend Developer</small><br></br><br></br>
+                
+                    
+          </div>
+          <div class="developer2-section">
+                <small> Tamira Siby..............................Backend Developer</small><br></br><br></br>
+                <small> Munisa Kholikova....................Backend Developer</small><br></br><br></br>
+                <small> Lee Hyeontaek........................Backend Developer</small><br></br><br></br>
+          </div>
+         
+          </div>
             </>
         );
     }
