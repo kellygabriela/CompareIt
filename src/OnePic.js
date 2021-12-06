abeb141244ed76692047a3fc0d2819dec34631df
nadiacandraa <79911218+nadiacandraa@users.noreply.github.com>
2021-12-06 18:30:16 +0900

    Update OnePic.js

--- src/OnePic.js
+++ src/OnePic.js
@@ -6,7 +6,7 @@ const OnePic = (props) => {
     return (
         <>
             {data
-            .slice(10, 11).map(item => (
+            .slice(11, 12).map(item => (
                     <li class="grid">
                         <h2>{item.title}</h2>
                         <img src={item.picture} alt=" "></img>
